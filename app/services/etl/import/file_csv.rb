049f36343229b906d2b129a8cdf5e8a039849164
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2025-09-10 16:07:29 -0300

    Add decimals to csv importer

--- app/services/etl/import/file_csv.rb
+++ app/services/etl/import/file_csv.rb
@@ -4,12 +4,13 @@ require 'ostruct'
 module Etl
   module Import
     class FileCsv
-      attr_accessor :timeframe, :ticker, :file_path, :bars
+      attr_accessor :timeframe, :ticker, :file_path, :bars, :decimals
 
-      def initialize(timeframe: "D1", ticker:, file_path:)
+      def initialize(timeframe: "D1", ticker:, file_path:, decimals: 2)
         @timeframe = timeframe
         @ticker = ticker
         @file_path = file_path
+        @decimals = decimals
         @bars = []
       end
 
@@ -38,11 +39,11 @@ module Etl
             timeframe:,
             ticker:,
             ts: row["Date"].to_date,
-            open: row["Open"].to_f.round(2),
-            high: row["High"].to_f.round(2),
-            low: row["Low"].to_f.round(2),
-            close: row["Close"].to_f.round(2),
-            aclose: row["Adj Close"].to_f.round(2),
+            open: row["Open"].to_f.round(self.decimals),
+            high: row["High"].to_f.round(self.decimals),
+            low: row["Low"].to_f.round(self.decimals),
+            close: row["Close"].to_f.round(self.decimals),
+            aclose: row["Adj Close"].to_f.round(self.decimals),
             volume: row["Volume"].to_i
           )
         end
